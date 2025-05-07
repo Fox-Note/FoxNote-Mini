8352e9acd0b7bb956d883dabbd8a4502ee612fab
fox-programmer <foxyfnaf.93yt@gmail.com>
2025-05-07 21:52:49 +0200

    Crete views

--- app/src/main/java/com/example/foxnotemini/MainActivity.kt
+++ app/src/main/java/com/example/foxnotemini/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.foxnotemini
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +16,11 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val button = findViewById<Button>(R.id.addNoteButton)
+        button.setOnClickListener {
+            setContentView(R.layout.view_notes)
         }
     }
 }
